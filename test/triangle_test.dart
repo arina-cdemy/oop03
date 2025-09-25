a45496efee9bdf2ffa64d46af447bbda33568fb1
arina-cdemy <arina.misakyan.cdemy@gmail.com>
2025-09-25 15:00:35 +0200

    remove unnecessary test

--- test/triangle_test.dart
+++ test/triangle_test.dart
@@ -2,33 +2,7 @@ import 'package:oop03/triangle.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('centimeter', () {
-    Triangle triangleInCm = Triangle.cm(10, 20);
-    expect(triangleInCm.heightInMm, 100);
-  });
-
-  test('decimeter', () {
-    Triangle triangleInDm = Triangle.dm(10, 20);
-    expect(triangleInDm.heightInMm, 1000);
-  });
-
-  test('meter', () {
-    Triangle triangleInM = Triangle.m(10, 20);
-    expect(triangleInM.heightInMm, 10000);
-  });
-
-  test('inch', () {
-    Triangle triangleInInch = Triangle.inch(10, 20);
-    expect(triangleInInch.heightInMm, 254);
-  });
-
-  test('feet', () {
-    Triangle triangleInFeet = Triangle.feet(10, 20);
-    expect(triangleInFeet.heightInMm, 3048);
-  });
-
-  //---
-  test('mm -> cm', () {
+    test('mm -> cm', () {
     Triangle triangleInMm = Triangle(10, 20);
     expect(triangleInMm.heightInCm, 1);
   });
